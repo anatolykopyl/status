12858d8d873247c3be89caf9cbe3dfe66594e7bd
Anatoly Kopyl <akopyl@radner.ru>
2021-01-17 02:01:03 +0300

    Обновил айди статусов

--- main.js
+++ main.js
@@ -1,6 +1,4 @@
 function showHint() {
-    console.log(document.getElementById("hint-div").style.display);
-
     if (document.getElementById("hint-div").style.display == "block") {
         document.getElementById("hint-div").style.display = "none";
         document.getElementById("hint-link").innerHTML = "Что это?";
@@ -10,4 +8,11 @@ function showHint() {
         document.getElementById("hint-link").innerHTML = "Понял";
         document.getElementById("hint-link").style.color = "black";
     }
+}
+
+var xhttp = new XMLHttpRequest();
+
+function send() {
+    xhttp.open("GET", "http://status.anatolykopyl.ru:5001/update", true);
+    xhttp.send();
 }
--- server/index.js
+++ server/index.js
@@ -10,16 +10,18 @@ dotenv.config();
 const app = Express();
 const port = process.env.PORT || 5001;
 
-const statusIds = [1, 2, 3, 4, 5, 9, 10, 17, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 38, 39];
+//const statusIds = [1, 2, 3, 4, 5, 9, 10, 17, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 38, 39];
+const statusIds = [114, 115, 116, 117, 118, 119, 120, 121];
 
 app.use(Cors());
 
 app.get('/update', (req, res) => {
     var id = statusIds[Math.floor(Math.random() * statusIds.length)];
-    let url = `https://api.vk.com/method/users.setCovidStatus?access_token=${process.env.TOKEN}&status_id=${id}&v=5.103`;
+    let url = `https://api.vk.com/method/status.setImage?access_token=${process.env.TOKEN}&status_id=${id}&v=5.103`;
     httpsReq(url).then((response) => {
         console.log(response);
-        res.redirect("https://status.anatolykopyl.ru");
+        //res.redirect("https://status.anatolykopyl.ru");
+        res.send();
     });
 });
 
