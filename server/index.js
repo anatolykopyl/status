ef94f64d212d7a086b27f9b6b7322697e04e1b82
Anatoly Kopyl <akopyl@radner.ru>
2021-01-19 02:29:30 +0300

    Изменил расположение токена

--- server/index.js
+++ server/index.js
@@ -11,13 +11,20 @@ const app = Express();
 const port = process.env.PORT || 5001;
 
 //const statusIds = [1, 2, 3, 4, 5, 9, 10, 17, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 38, 39];
-const statusIds = [114, 115, 116, 117, 118, 119, 120, 121];
+const statusIds = [114, 115, 116, 117, 118, 119, 120, 121]; //MLP статусы
 
 app.use(Cors());
 
 app.get('/update', (req, res) => {
     var id = statusIds[Math.floor(Math.random() * statusIds.length)];
-    let url = `https://api.vk.com/method/status.setImage?access_token=${process.env.TOKEN}&status_id=${id}&v=5.103`;
+    let token;
+    fs.readFile('../get-vk-token/token.txt', 'utf8', function(error, data) {
+      if (error) {
+        console.log(error);
+      }
+      token = data;
+    });
+    let url = `https://api.vk.com/method/status.setImage?access_token=${token}&status_id=${id}&v=5.103`;
     httpsReq(url).then((response) => {
         console.log(response);
         //res.redirect("https://status.anatolykopyl.ru");
